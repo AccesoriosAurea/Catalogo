7048e70a0155cd35d6cd06846287a8dcbda32db4
AccesoriosAurea <aurea202501@gmail.com>
2026-04-12 20:11:55 -0500

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,25 @@
+// Archivo: js/productos.js
+// Únicamente datos de productos
+
+const productosData = {
+  anillos: [],
+  
+  collares: [],
+  
+  pulseras: [],
+  
+  aretes: [
+    {
+      id: 'ARE-ACE-F-001', 
+      codigo: 'ARE-ACE-F-001', 
+      nombre: 'Candongas Doradas', 
+      desc: 'Diseño brillante', 
+      precio: 13000, 
+      imagen: 'images/aretes/ARE-ACE-F-001.jpg',
+      icono: 'fa-circle-notch',
+      cat: 'aretes', 
+      material: 'acero',
+      sexo: 'F'
+    }
+  ]
+};
