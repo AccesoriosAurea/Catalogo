95092027caa30915738e7df6801a8084f9727847
AccesoriosAurea <aurea202501@gmail.com>
2026-04-12 21:59:43 -0500

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -20,6 +20,18 @@ const productosData = {
       cat: 'aretes', 
       material: 'acero',
       sexo: 'F'
+    },
+    {
+      id: 'ARE-ROD-F-001', 
+      codigo: 'ARE-ROD-F-001', 
+      nombre: 'Aretes florales', 
+      desc: 'Diseño brillante', 
+      precio: 13000, 
+      imagen: 'images/aretes/ARE-ROD-F-001.jpg',
+      icono: 'fa-circle-notch',
+      cat: 'aretes', 
+      material: 'rodio',
+      sexo: 'F'
     }
   ]
 };
